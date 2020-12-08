11074860a4a1f153bb75966895c5b86820137726
deeborges <deyvisonsilvaborges@gmail.com>
2020-12-08 02:43:41 -0300

    feat(test): setup pro jest

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.js(x)'],
+  collectCoverageFrom: ['src/**/*.js(x)', '!src/**/*.stories.js'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.js']
 };
