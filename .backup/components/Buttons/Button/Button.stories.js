7177258a9e23b3ae559583e4ea83f7e64d05f4ea
deeborges <deyvisonsilvaborges@gmail.com>
2021-01-02 23:01:03 -0300

    fix : deleteds

--- .backup/components/Buttons/Button/Button.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Button from './index';
-
-export default {
-  title: 'Components/Button',
-  component: Button
-};
-
-export const usage = () => <Button>Meu botão</Button>;
